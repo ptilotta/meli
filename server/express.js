1d8bbbbb038e4596fbb3d87cb056e1066cd3834d
Pablo Tilotta <pablotilotta@vb-mundo.com>
2018-07-16 14:36:47 -0300

    Commit

--- server/express.js
+++ server/express.js
@@ -5,7 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-app.use(express.static(__dirname + '/public')); // se define un Middleware general
+app.use(express.static('../public')); // se define un Middleware general
 app.use(bodyParser.urlencoded({ extended: false })) // Convierte la URL en formato correcto
 app.use(bodyParser.json()); // Convierte el Body en un formato JSON
 app.use(require('../routes/rutas')); // el archivo rutas.js procesa los POSTs y GET
