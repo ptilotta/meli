734a917485ba467e22a2415529123337ddf83cfc
Pablo Tilotta <pablotilotta@vb-mundo.com>
2018-07-15 21:30:43 -0300

    first commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+// ===========================================================================
+//   Examen BackEnd Mercadolibre 
+//   Autor : Pablo Tilotta
+//   Descripción : Se configura un RestServer que responderá a peticiones
+//                 GET y POST, con 2 servicios configurados para evaluar
+//                 una matriz de valores de ADN posibles, determinando 
+//                 si corresponde a un Mutante o a un Humano. 
+//                 Se programa el alojamiento de datos en MONGODB alojada
+//                 en el servicio gratuito MLAB, y se sube el proyecto NODE 
+//                 a HEROKU, además de publicar en GITHUB el proyecto.
+//
+//   Paquetes usados : Express, Mongoose, Underscore, Body-Parser, 
+//                     Mongoose-Unique-Validator.
+//   Período : Julio 2018
+// ===========================================================================
