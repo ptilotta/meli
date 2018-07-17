22d477b83e9d1f19bb9b78ea3dbb4a012a767b98
Pablo Tilotta <pablotilotta@vb-mundo.com>
2018-07-17 20:49:30 -0300

    Commit

--- config/config.js
+++ config/config.js
@@ -21,6 +21,7 @@ process.env.URLDB = urlDB;
 
 if (!process.env.NODE_ENV === 'dev') {
 
+    console.log('Antes de Connect de Mongoose');
     mongoose.connect(process.env.URLDB, { useNewUrlParser: true }, (err, res) => {
         if (err) throw err;
         console.log('Base de Datos ONLINE');
