36b4aa82480ed57001bb078d64d4e46b287556ad
Pablo Tilotta <pablotilotta@vb-mundo.com>
2018-07-17 13:37:07 -0300

    Commit

--- config/config.js
+++ config/config.js
@@ -10,7 +10,14 @@ process.env.PORT = process.env.PORT || 3000;
 //   Conexión a MONGODB
 // --------------------------------------------------------
 
+const mongoose = require('mongoose');
+
 let urlDB;
 
 urlDB = process.env.MongoURI;
 process.env.URLDB = urlDB;
+
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true }, (err, res) => {
+    if (err) throw err;
+    console.log('Base de Datos ONLINE');
+})
