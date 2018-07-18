91f45a3bd3f98dafc280bbd58da7aa68f7ec058a
Pablo Tilotta <pablotilotta@vb-mundo.com>
2018-07-17 23:01:25 -0300

    Commit

--- routes/rutas.js
+++ routes/rutas.js
@@ -93,10 +93,11 @@ app.post('/mutant', function(req, res) {
         if (err) {
             console.log(`ValidationError ${err.ValidationError}`);
             let mensajeError = err.ValidationError;
-            if (!mensajeError.includes("adn debe de ser único")) {
+            if (mensajeError.includes("adn debe de ser único")) {} else {
                 console.log('Hubo error en MONGODB', err);
                 return res.status(400).json({ err });
             }
+            console.log(err);
         }
     });
 
