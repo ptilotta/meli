7eea78f3910fb4f189cf5aec7619858131622d4a
Pablo Tilotta <pablotilotta@vb-mundo.com>
2018-07-22 12:58:39 -0300

    Commit

--- funciones/funciones.js
+++ funciones/funciones.js
@@ -198,9 +198,10 @@ function checkMutante(t) {
     // misma linea contenga mas de un patron
     let aBuscar = ['AAAA', 'CCCC', 'GGGG', 'TTTT'];
     let conteo = 0;
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 4; i++) {
         let busca = new RegExp(aBuscar[i], "g");
         let campo = t.match(busca);
+        console.log(campo);
         if (!typeof campo === undefined) {
             let texto = campo.split(',');
             conteo += texto.length;
