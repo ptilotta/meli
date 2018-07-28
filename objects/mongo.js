a0088af8d16abdf30a109abbba1464e845cc2670
Pablo Tilotta <pablotilotta@vb-mundo.com>
2018-07-28 17:15:35 -0300

    Commit

--- objects/mongo.js
+++ objects/mongo.js
@@ -27,11 +27,8 @@ class Mongo {
 
         /* Crea un Schema para este objeto */
         this.uniqueMs = uniqueMsg;
-        let Esquema = new mongoose.Schema;
-        let sch = new Esquema(schema);
-
-        sch.plugin(uniqueValidator, { message: uniqueMsg });
-        this.modelo = mongoose.model(nombre, sch);
+        schema.plugin(uniqueValidator, { message: uniqueMsg });
+        this.modelo = mongoose.model(nombre, schema);
     }
 
     async Connect() {
