239e7cd650c8bcbe24873382b67e1a49c180a387
Pablo Tilotta <pablotilotta@vb-mundo.com>
2018-07-29 01:27:52 -0300

    Commit

--- objects/mutant.js
+++ objects/mutant.js
@@ -11,8 +11,10 @@ class Mutant {
     }
 
     async graboMutant(adn, mutante) {
+        console.log('1. Inicio GraboMutant');
         let mongoMutant = new Mongo(process.env.MongoURI);
         mongoMutant.Connect().then(() => {
+            console.log('2. Terminó de hacer el Connect()');
             if (mongoMutant.error === true) {
                 this.error = true;
                 this.mensaje = mongoMutant.mensaje;
@@ -20,16 +22,19 @@ class Mutant {
             }
 
             // Seteo Schema MUTANT
+            console.log('3. Voy a crear el Schema MUTANT');
             mongoMutant.AddSchema('MUTANT', {
                 dna: { type: String, required: [true, 'Campo dna Requerido'] },
                 mutante: { type: Boolean, required: [true, 'Campo mutante Requerido'] }
             }, process.env.MSGUNIQUE).then(() => {
 
                 // Grabo los datos en la Base de Datos
+                console.log('4. Antes de ir a Save');
                 mongoMutant.Save({
                     dna: adn,
                     mutante
                 }).then(() => {
+                    console.log('5. despues de Save');
                     console.log(`mongoMutant.error = ${mongoMutant.error}`);
                     if (mongoMutant.error === true) {
                         console.log('Dio Error mongoMutant.Save !!!', this.mensaje);
@@ -41,6 +46,7 @@ class Mutant {
             });
         });
 
+        console.log('6. Fin de GraboMutant');
         this.error = false;
         this.mensaje = {};
     }
