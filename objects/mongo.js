3704b72640d8738d04435703d47b4f2e3ae75550
Pablo Tilotta <pablotilotta@vb-mundo.com>
2018-07-28 21:52:19 -0300

    Commit

--- objects/mongo.js
+++ objects/mongo.js
@@ -61,7 +61,7 @@ class Mongo {
             return;
         }
         let sch = new this.modelo(datos);
-
+        console.log(`Los datos son ${JSON.stringify(dato)}`);
         sch.save((err, results) => {
             this.error = false;
             this.mensaje = {};
