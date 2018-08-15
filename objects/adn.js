a777c6c643ed5c2ac2e36efd6e1b0de1e5c001ec
Pablo Tilotta <pablotilotta@vb-mundo.com>
2018-08-14 21:13:38 -0300

    Ult Commit

--- objects/adn.js
+++ objects/adn.js
@@ -20,24 +20,33 @@ class Adn {
         this.mensaje = {};
         this.mensajeError = {};
         this.camposMutantes = 0;
+        this.adn = adn;
+        this.x = 0;
+        this.y = 0;
+        this.iteroporFila = false;
+        this.ultCaracter = "";
+        this.iterar = true;
+        this.tipo = "";
+        this.desde = 0;
+        this.desde2 = 0;
+        this.incrementoColumna = 0;
+        this.incrementoFila = 0;
+        this.contador = 1;
+    };
 
-        // Creo la matriz desde el JSON recibido
-        for (var i in adn) {
-            this.tabla.push(adn[i]);
+    creoTabla() {
+        for (var i in this.adn) {
+            this.tabla.push(this.adn[i]);
         }
     }
 
     validaciones() {
-        if (!this.validoLongitudValida()) {
-            return false;
-        };
-        if (!this.chequeoLetrasValidas()) {
-            return false;
-        };
+        if (!this._validoLongitudValida()) { return false; };
+        if (!this._chequeoLetrasValidas()) { return false; };
         return true;
     }
 
-    validoLongitudValida() {
+    _validoLongitudValida() {
         let longitudElemento = 0;
         let longitud = 0;
         this.error = false;
@@ -58,7 +67,7 @@ class Adn {
         return true;
     }
 
-    chequeoLetrasValidas() {
+    _chequeoLetrasValidas() {
         this.error = false;
         this.mensajeError = {};
         for (var i = 0; i < this.tabla.length; i++) {
@@ -76,126 +85,156 @@ class Adn {
     }
 
     isMutant() {
-
-        this.recorroMatriz("H", 0, 0, 0, 0); // Recorrido Horizontal
-        if (this.camposMutantes >= this.ocurrencias) { return true; }
-
-        this.recorroMatriz("V", 0, 0, 0, 0); // Recorrido Vertical
-        if (this.camposMutantes >= this.ocurrencias) { return true; }
-
-        this.recorroMatriz("D", 0, 0, 1, 1); // Recorrido Diagonal - Derecha a Izquierda
-        if (this.camposMutantes >= this.ocurrencias) { return true; }
-
-        this.recorroMatriz("D", 0, 0, 1, -1); // Recorrido Diagonal - Izquierda a Derecha 
-        if (this.camposMutantes >= this.ocurrencias) { return true; }
-
+        let Matriz = [
+            ["H", 0, 0, 0, 0],
+            ["V", 0, 0, 0, 0],
+            ["D", 0, 0, 1, 1],
+            ["D", 0, 0, 1, 1]
+        ];
+        for (var i = 0; i < 4; i++) {
+            this.tipo = Matriz[i][0];
+            this.desde = Matriz[i][1];
+            this.desde2 = Matriz[i][2];
+            this.incrementoFila = Matriz[i][3];
+            this.incrementoColumna = Matriz[i][4];
+            this._recorroMatriz();
+            if (this.camposMutantes >= this.ocurrencias) { return true; }
+        }
         return false;
     }
 
-    recorroMatriz(tipo, desde, desde2, incrementoFila, incrementoColumna) {
-        var contador = 1,
-            x, y, actual_y, actual_x, iteroporFila = false,
-            ultCaracter = '',
-            t, iterar = true;
-
-        if (tipo === "V") {
-            y = desde2
-            x = desde
-            actual_y = desde2
-            actual_x = desde
-        } else {
-            y = desde
-            x = desde2
-            actual_y = desde
-            actual_x = desde2
+    _asignoInicio() {
+        if (this.tipo === "V") {
+            this.y = this.desde2;
+            this.x = this.desde;
+            return;
         }
+        this.y = this.desde;
+        this.x = this.desde2;
+    }
+
+    _recorroMatriz() {
+        var t = "";
+        this._asignoInicio();
 
         while (true) {
-            ultCaracter = "";
-            iterar = true;
-            while (iterar) {
-                t = this.tabla[y].substr(x, 1);
-                if (ultCaracter === t) {
-                    contador += 1;
-                } else {
-                    ultCaracter = t;
-                    contador = 1;
-                }
-                if (contador === this.largoString) {
-                    this.camposMutantes += 1;
-                    if (this.camposMutantes >= this.ocurrencias) {
-                        return;
-                    }
-                    contador = 0;
-                    ultCaracter = "";
-                }
-                switch (tipo) {
-                    case "H":
-                        x += 1;
-                        if (x > (this.tabla[y].length - 1)) {
-                            iterar = false;
-                        }
-                        break;
-                    case "V":
-                        y += 1;
-                        if (y > (this.tabla.length - 1)) {
-                            iterar = false;
-                        }
-                        break;
-                    case "D":
-                        x += incrementoColumna;
-                        y += incrementoFila;
-                        if (y > this.tabla.length - 1) {
-                            iterar = false;
-                            break;
-                        }
-                        if (x < 0 || y < 0 || x > (this.tabla[y].length - 1) || y > this.tabla.length) {
-                            iterar = false;
-                        }
-                        break;
-                }
-            }
-            switch (tipo) {
-                case "V":
-                    actual_y = desde;
-                    actual_x += 1;
-                    if (actual_x > (this.tabla[0].length - 1)) {
-                        return;
-                    }
-                    break;
-                case "H":
-                    actual_y += 1;
-                    actual_x = desde2;
-                    if (actual_y > (this.tabla.length - 1)) {
-                        return;
-                    }
-                    break;
-                case "D":
-                    if (iteroporFila === false) {
-                        actual_x = actual_x + Math.abs(incrementoColumna)
-                        actual_y = desde
-                        if (actual_x > (this.tabla[0].length - 1)) {
-                            iteroporFila = true;
-                        }
-                    }
-                    if (iteroporFila = true) {
-                        if (incrementoColumna < 0) {
-                            actual_x = this.tabla[0].length - 1;
-                        } else {
-                            actual_x = desde2;
-                        }
-                        actual_y += Math.abs(incrementoFila);
-                    }
-
-                    if (actual_y > this.tabla.length - 1) {
-                        return;
-                    }
-                    break;
-            }
-            y = actual_y;
-            x = actual_x;
+            this.ultCaracter = "";
+            this.iterar = true;
+            if (!this._iterarMatriz()) { return; };
+            if (!this._IncrementoParametros()) { return; };
         };
     };
+
+    _iterarMatriz() {
+        while (this.iterar) {
+            t = this.tabla[y].substr(x, 1);
+            this._comparoCaracter();
+            if (this._metaAlcanzada()) {
+                return false;
+            }
+            this.iterar = this._muevoCursor();
+        }
+    }
+
+    _IncrementoParametros() {
+        if (this.tipo === "V") {
+            this.y = this.desde;
+            if (!this._incrementoX()) { return false; }
+        };
+        if (this.tipo === "H") {
+            this.x = this.desde2;
+            if (!this._incrementoY()) { return false; }
+        };
+        if (this.tipo === "D") {
+            if (!this._incrementoXY()) { return false; }
+        }
+        return true;
+    }
+
+    _incrementoY() {
+        this.y += 1;
+        if (this.y > (this.tabla.length - 1)) {
+            return false;
+        }
+        return true;
+    }
+
+    _incrementoX() {
+        this.x += 1;
+        if (this.x > (this.tabla[0].length - 1)) {
+            return false;
+        }
+        return true;
+    }
+
+    _incrementoDiag() {
+        this.x += this.incrementoColumna;
+        this.y += this.incrementoFila;
+        if (this.x < 0 || this.y < 0 || this.x > (this.tabla[0].length - 1) || this.y > this.tabla.length) { return false; }
+        return true;
+    }
+
+    _incrementoXY() {
+        if (this.iteroporFila === false) {
+            this._iteroPorColumna();
+        } else {
+            this._iteroPorFila();
+        }
+        if (this.y > this.tabla.length - 1) {
+            return false;
+        }
+        return true;
+    }
+
+    _iteroPorColumna() {
+        this.x = this.x + Math.abs(this.incrementoColumna);
+        this.y = this.desde;
+        if (this.x > (this.tabla[0].length - 1)) {
+            this.iteroporFila = true;
+        }
+    }
+
+    _iteroPorFila() {
+        if (this.incrementoColumna < 0) {
+            this.x = this.tabla[0].length - 1;
+        } else {
+            this.x = this.desde2;
+        }
+        this.y += Math.abs(this.incrementoFila);
+    }
+
+    _comparoCaracter() {
+        if (this.ultCaracter === t) {
+            this.contador += 1;
+        } else {
+            this.ultCaracter = t;
+            this.contador = 1;
+        }
+    }
+
+    _metaAlanzada() {
+        if (this.contador === this.largoString) {
+            this.camposMutantes += 1;
+            if (this.camposMutantes >= this.ocurrencias) {
+                return true;
+            }
+            contador = 0;
+            ultCaracter = "";
+        }
+        return false;
+    }
+    _muevoCursor() {
+        if (this.tipo === "V") {
+            if (!this._incrementoY()) { return false; }
+        };
+        if (this.tipo === "H") {
+            if (!this._incrementoX()) { return false; }
+        };
+        if (this.tipo === "D") {
+            if (!this._incrementoDiag()) { return false; }
+        }
+        return true;
+    }
 };
 
 module.exports = Adn;
